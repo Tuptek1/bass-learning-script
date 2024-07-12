46e69a1e0e1a35be283181122b40fa018f5f24e2
Tuptek1 <tuptek112@gmail.com>
2024-07-12 12:14:20 +0200

    finished the script

---
+++ bass_notes.py
@@ -0,0 +1,13 @@
+notes_dict = {"A" : "2nd fret on G, 7th fret on D, open string on A, 12th fret on A, 5th fret on E",
+              "A#": "3rd fret on G, 8th fret on D, 1st fret on A, 6th fret on E",
+              "B": "4th fret on G, 9th fret on D, 2nd fret on A, 7th fret on E",
+              "C" : "5th fret on G, 10th fret on D, 3rd fret on A, 8th fret on E",
+              "C#" : "6th fret on G, 11th fret on D, 4th fret on A, 9th fret on E",
+              "D" : "7th fret on G, open string on D, 12th fret on D, 5th fret on A, 10th fret on E",
+              "D#" : "8th fret on G, 1st fret on D, 6th fret on A, 11th fret on E",
+              "E" : "9th fret on G, 2nd frest on D, 7th fret on A, open string on E, 12th fret on E",
+              "F" : "10th fret on G, 3rd fret on D, 8th fret on A, 1st fret on E",
+              "F#" : "11th fret on G, 4th fret on D, 9th fret on A, 2nd fret on E",
+              "G" : "open string on G, 12th fret on G, 5th fret on D, 10th fret on A, 3rd fret on E",
+              "G#" : "1st fret on G, 6th fret on D, 11th fret on A, 4th fret on E",
+              }
---
+++ main.py
@@ -0,0 +1,12 @@
+from bass_notes import notes_dict
+import random, time
+
+while True:
+    notes_list = [i for i in notes_dict]
+    current_note = random.choice(notes_list)
+    print(current_note)
+    time.sleep(15)
+    print(notes_dict.get(current_note))
+    print(
+        "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |"
+    )
