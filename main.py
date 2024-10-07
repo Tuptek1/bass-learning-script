f8e5a42f5ff56e7f14936fb84fee01a349c73cdc
Tuptek <tuptek112@gmail.com>
2024-10-07 21:19:59 +0200

    added name==main statement

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from bass_notes import notes_dict
 import random, time
 
-while True:
-    notes_list = [i for i in notes_dict]
-    current_note = random.choice(notes_list)
-    print(current_note)
-    time.sleep(15)
-    print(notes_dict.get(current_note))
-    print(
-        "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |"
-    )
+def main():
+    while True:
+        notes_list = [i for i in notes_dict]
+        current_note = random.choice(notes_list)
+        print(current_note)
+        time.sleep(15)
+        print(notes_dict.get(current_note))
+        print(
+            "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |"
+        )
+
+if __name__ == '__main__':
+    main()
